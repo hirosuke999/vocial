d3b0140b48fb878ba86c56ac851aa175e3cf0466
pt.yu-senga <pt.yu-senga@tgl.co.jp>
2019-08-02 17:58:47 +0900

    update

--- lib/vocial_web/controllers/poll_controller.ex
+++ lib/vocial_web/controllers/poll_controller.ex
@@ -2,6 +2,7 @@ defmodule VocialWeb.PollController do
   use VocialWeb, :controller
 
   alias Vocial.Votes
+  alias VocialWeb.Router.Helpers, as: Routes
 
   def index(conn, _params) do
     polls = Votes.list_polls()
@@ -11,5 +12,15 @@ defmodule VocialWeb.PollController do
   def new(conn, _params) do
     poll = Votes.new_poll()
     render(conn, "new.html", poll: poll)
+  end
+
+  def create(conn, %{"poll" => poll_params, "options" => options}) do
+    split_options = String.split(options, ",")
+
+    with {:ok, _poll} <- Votes.create_poll_with_options(poll_params, split_options) do
+      conn
+      |> put_flash(:info, "Poll created successfully!")
+      |> redirect(to: Routes.poll_path(conn, :index))
+    end
   end
 end
