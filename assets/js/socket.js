17e6bd4ffac788dbd3ded571cd4e4c3e79307152
pt.yu-senga <pt.yu-senga@tgl.co.jp>
2019-09-17 10:28:27 +0900

    add pushVote func's implementation on ch.6

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,5 +1,22 @@
 import { Socket } from 'phoenix'
 
+const pushVote = (el, channel) => {
+  channel
+    .push('vote', { option_id: el.getAttribute('data-option-id') })
+    .receive('ok', res => console.log('You Voted!'))
+    .receive('error', res => console.log('Failed to vote:', res))
+}
+
+const onJoin = (res, channel) => {
+  document.querySelectorAll('.vote-button-manual').forEach(el => {
+    el.addEventListener('click', event => {
+      event.preventDefault()
+      pushVote(el, channel)
+    })
+  })
+  console.log('Joined channel:', res)
+}
+
 const socket = new Socket('/socket')
 socket.connect()
 
@@ -7,7 +24,7 @@ if (document.getElementById('enable-polls-channel')) {
   const channel = socket.channel('polls:lobby', {})
   channel
     .join()
-    .receive('ok', res => console.log('Joined channel:', res))
+    .receive('ok', res => onJoin(res))
     .receive('error', res => console.log('Failed to join channel:', res))
 
   document.getElementById('polls-ping').addEventListener('click', () => {
