aec94101ccdc4e1e9614a096fb3032dd9969c2bb
pt.yu-senga <pt.yu-senga@tgl.co.jp>
2019-09-17 11:10:57 +0900

    add implementation for live voting

--- lib/vocial_web/channels/polls_channel.ex
+++ lib/vocial_web/channels/polls_channel.ex
@@ -6,7 +6,17 @@ defmodule VocialWeb.PollsChannel do
   end
 
   def handle_in("ping", _payload, socket) do
-    broadcast socket, "pong", %{message: "pong"}
+    broadcast(socket, "pong", %{message: "pong"})
     {:reply, {:ok, %{message: "pong"}}, socket}
+  end
+
+  def handle_in("vote", %{"option_id" => option_id}, socket) do
+    with {:ok, option} <- Vocial.Votes.vote_on_option(option_id) do
+      broadcast(socket, "new_vote", %{"option_id" => option_id, "votes" => option.votes})
+      {:reply, {:ok, %{"option_id" => option_id, "votes" => option.votes}}, socket}
+    else
+      {:error, _} ->
+        {:reply, {:error, %{"message" => "Failed to vote for option!"}}, socket}
+    end
   end
 end
